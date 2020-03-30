75880b6a4e69310163bdc2c73df23512294eaab0
thienqui-communistspy <professionalbusiness505@gmail.com>
2020-03-30 16:29:29 -0400

    Added ability to add new stuff

--- flasktodo/todos.py
+++ flasktodo/todos.py
@@ -1,14 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from . import db
 
 
 bp = Blueprint("todos", __name__)
 
-@bp.route("/")
+@bp.route("/", methods=('GET', 'POST'))
 def index():
     """View for home page which shows list of to-do items."""
 
+    if request.method == 'POST':
+        todo = request.form['todo']
+        with db.get_db() as con:
+            with con.cursor() as cur:
+                cur.execute("INSERT INTO todos(description, completed, created_at) VALUES(%s, %s, NOW())",
+                (todo, False)
+                )
     cur = db.get_db().cursor()
     cur.execute('SELECT * FROM todos')
     todos = cur.fetchall()
